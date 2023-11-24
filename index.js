d973d4809947eead09cba052fb67220792a1907a
Raziur-Rahman <mdraziurrahman051@gmail.com>
2023-11-25 01:42:38 +0600

    Create Users Database Collection and Implemented post, get function of Users Collection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -9,9 +11,66 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=>{
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idotoa5.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.DB_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const usersCollection = client.db('TechDiscoveriaDB').collection('users');
+
+
+        // User's Api is here
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+
+            const query = {
+                email: user.email
+            }
+
+            const existUser = await usersCollection.findOne(query);
+            if (existUser) {
+                res.send({ massage: "User Already Exist", insertedId: null })
+            }
+            else {
+                const result = await usersCollection.insertOne(user);
+                res.send(result);
+            }
+
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send("Tech Discoveria Server is running....");
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log("Discoveria is Running at port: ", port)
 })
